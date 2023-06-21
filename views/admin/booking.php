d97ab9eccaa3e2248272d25cc301f7e51df936c7
leon <leon00albert@gmail.com>
2023-06-21 11:04:31 +0300

    refactor: refactor template in bookingadmin.php

--- views/admin/booking.php
+++ views/admin/booking.php
@@ -14,66 +14,65 @@ if (
                 <div class="container">
                     <div class="card m-3 card-shadow">
                         <div class="card-body">
-                        <h2>Bookings</h2>
-                <table class="table">
-                    <tr>
-                        <th>
-                            ID
-                        </th>
-                        <th>
-                            Car id
-                        </th>
-                        <th>
-                            Car
-                        </th>
-                        <th>
-                            Name
-                        </th>
-                        <th>
-                            Date
-                        </th>
+                            <h2>Bookings</h2>
+                            <table class="table">
+                                <tr>
+                                    <th>
+                                        ID
+                                    </th>
+                                    <th>
+                                        Car id
+                                    </th>
+                                    <th>
+                                        Car
+                                    </th>
+                                    <th>
+                                        Name
+                                    </th>
+                                    <th>
+                                        Date
+                                    </th>
 
-                        <th>
-                            Slot
-                        </th>
-                        <th>
-                        </th>
-                    </tr>
-                    <?php
-                    $bookings = $controller->Get("bookings");
+                                    <th>
+                                        Slot
+                                    </th>
+                                    <th>
+                                    </th>
+                                </tr>
+                                <?php
+                                $bookings = $controller->Get("bookings");
+                                foreach ($bookings as $booking) :
+                                    $booking["date"] = json_decode($booking["date"], true);
+                                ?>
+                                    <tr>
+                                        <td><?= $booking["id"] ?></td>
+                                        <td><?= $booking["carId"] ?></td>
+                                        <td><?= $booking["car"] ?></td>
+                                        <td><?= $booking["name"] ?></td>
+                                        <td><?= $booking["date"]["date"] ?></td>
 
-                    foreach ($bookings as $booking) :
-      
-                        $booking["date"] = json_decode($booking["date"],true);
-                        echo <<<EOD
-                        <tr>
-                            <td>{$booking["id"]}</td>
-                            <td>{$booking["carId"]}</td>
-                            <td>{$booking["car"]}</td>
-                            <td>{$booking["name"]}</td>
-                            <td>{$booking["date"]["date"]}</td>
-                    EOD;
-                        echo "<td>";
-                        foreach ($booking["date"]["slots"] as $slot) {
-                            if ($slot["available"] === false) {
-                                echo $slot["name"];
-                            }
-                        }
-                        echo "</td>";
-                        echo "<td>
-            <form action=\"/app/Controller.php\" method=\"POST\">
-            <input type=\"hidden\" name=\"type\" value=\"bookings\" />
-            <input type=\"hidden\" name=\"action\" value=\"delete\" />
-                    <input type=\"hidden\" name=\"id\" value=\"{$booking["id"]}\" />
-         <button type=\"submit\" class=\"btn btn-danger\">
-      
-            Delete</button>
-                  </form>
- 
-                </tr>";
-                    endforeach;
-                    ?>
-                </table>
+                                        <td>
+                                            <?php foreach ($booking["date"]["slots"] as $slot) {
+                                                if ($slot["available"] === false) {
+                                                    echo $slot["name"];
+                                                }
+                                            }
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <form action="/app/Controller.php" method="POST">
+                                                <input type="hidden" name="type" value="bookings" />
+                                                <input type="hidden" name="action" value="delete" />
+                                                <input type="hidden" name="id" value="<?= $booking["id"] ?>" />
+                                                <button type="submit" class="btn btn-danger">
+
+                                                    Delete</button>
+                                            </form>
+
+                                    </tr>
+                                <?php endforeach; ?>
+
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -81,10 +80,7 @@ if (
         </div>
     </div>
 
-    <?php include "footer.php";?>
+    <?php include "footer.php"; ?>
 <?php } else {
     header("location: /views/login.php");
 } ?>
-       
-       
-
